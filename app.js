1e605a0ac7441ab51dba8c09a71519b0afbcb7ac
Ilona Melcher <ilona.melcher8@gmail.com>
2024-09-23 18:22:30 +0200

    szubienica v2

---
+++ app.js
@@ -0,0 +1,38 @@
+// window.onload = start(letters);
+// window.onload = async function () {
+//   start(letters);
+// };
+
+// const password = randomSentence(sentence);
+
+let shownPassword = "";
+let failureCounter = 0;
+const yes = new Audio("yes.wav");
+const no = new Audio("no.wav");
+
+const data = fetch("https://api.gameofthronesquotes.xyz/v1/random")
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res.sentence);
+    const password = res.sentence;
+
+    for (i = 0; i < password.length; i++) {
+      if (password.charAt(i) == " ") {
+        shownPassword = shownPassword + " ";
+      } else if (password.charAt(i) == ",") {
+        shownPassword = shownPassword + ",";
+      } else if (password.charAt(i) == ".") {
+        shownPassword = shownPassword + ".";
+      } else if (password.charAt(i) == "'") {
+        shownPassword = shownPassword + "'";
+      } else if (password.charAt(i) == "?") {
+        shownPassword = shownPassword + "?";
+      } else if (password.charAt(i) == "!") {
+        shownPassword = shownPassword + "!";
+      } else {
+        shownPassword = shownPassword + "-";
+      }
+    }
+
+    start(letters, password.toUpperCase());
+  });
---
+++ functions.js
@@ -0,0 +1,85 @@
+function randomSentence(arr) {
+  const rand = Math.floor(Math.random() * (arr.length - 1) + 1);
+  return arr[rand].toUpperCase();
+}
+
+String.prototype.setChar = function (place, char) {
+  if (place > this.length - 1) {
+    return this.toString();
+  } else return this.substr(0, place) + char + this.substr(place + 1);
+};
+
+function start(letterChar, password) {
+  const letters = document.getElementById("letters");
+
+  for (i = 0; i < letterChar.length; i++) {
+    const letter = document.createElement("div");
+    letter.classList.add("letter");
+    letter.id = `lett${i}`;
+    letter.innerHTML = letterChar[i];
+    letters.appendChild(letter);
+
+    letter.addEventListener("click", (e) => {
+      check(e.target.id.substr(4), password);
+    });
+
+    if ((i + 1) % 7 == 0) {
+      const clearBoth = document.createElement("div");
+      clearBoth.style.clear = "both";
+      letters.appendChild(clearBoth);
+    }
+  }
+
+  showPassword();
+}
+
+function check(nr, password) {
+  let hit = false;
+
+  for (i = 0; i < password.length; i++) {
+    if (password.charAt(i) == letters[nr]) {
+      shownPassword = shownPassword.setChar(i, letters[nr]);
+      hit = true;
+    }
+  }
+
+  if (hit == true) {
+    yes.play();
+    const element = "lett" + nr;
+    document.getElementById(element).style.background = "#003300";
+    document.getElementById(element).style.color = "#00c000";
+    document.getElementById(element).style.border = "3px solid #00c000";
+    document.getElementById(element).style.cursor = "default";
+    showPassword();
+  } else {
+    no.play();
+    const element = "lett" + nr;
+    document.getElementById(element).style.background = "#330000";
+    document.getElementById(element).style.color = "#c00000";
+    document.getElementById(element).style.border = "3px solid #c00000";
+    document.getElementById(element).style.cursor = "default";
+    document.getElementById(element).setAttribute("onclick", ";");
+
+    //skucha
+    failureCounter++;
+    let picture = "img/s" + failureCounter + ".jpg";
+    document.getElementById("gallows").innerHTML =
+      '<img src="' + picture + '" alt="" />';
+  }
+  //wygrana
+  if (password == shownPassword)
+    document.getElementById("letters").innerHTML =
+      "Wygrana. Poprawne  hasło: " +
+      password +
+      '<br/><br/><span class="reset" onclick="location.reload()">JESZCZE RAZ? </span> ';
+  //przegrana
+  if (failureCounter >= 9)
+    document.getElementById("letters").innerHTML =
+      "Przegrana. Poprawne  hasło: " +
+      password +
+      '<br/><br/><span class="reset" onclick="location.reload()">JESZCZE RAZ? </span> ';
+}
+
+function showPassword() {
+  document.getElementById("board").innerHTML = shownPassword;
+}
